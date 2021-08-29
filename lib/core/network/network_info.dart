19fec24b731f3b26f74de1b97a8a47fdb123094f
pavandubey <ikroop.singh.kalsi@gmail.com>
2021-08-29 17:06:00 +0530

    implement network info

--- lib/core/network/network_info.dart
+++ lib/core/network/network_info.dart
@@ -1,4 +1,14 @@
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+
 abstract class NetworkInfo {
   Future<bool> get isConnected;
 }
 
+class NetworkInfoImplementation implements NetworkInfo {
+  final InternetConnectionChecker connectionChecker;
+
+  NetworkInfoImplementation(this.connectionChecker);
+
+  @override
+  Future<bool> get isConnected => connectionChecker.hasConnection;
+}
