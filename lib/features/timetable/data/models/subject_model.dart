dbc430108d11cc1746eff1d301b5cee563788cf8
pavandubey <ikroop.singh.kalsi@gmail.com>
2021-08-29 18:05:30 +0530

    implement json conversion in SubjectModel

--- lib/features/timetable/data/models/subject_model.dart
+++ lib/features/timetable/data/models/subject_model.dart
@@ -1,8 +1,25 @@
 import 'package:pretend/features/timetable/domain/entities/subject.dart';
 
+class SubjectModelFields {
+  static const NAME = 'name';
+  static const CODE = 'code';
+}
+
 class SubjectModel extends Subject {
-  const SubjectModel({
-    required String name,
-    required String code
-  }) : super(name, code);
+  const SubjectModel({required String name, required String code})
+      : super(name, code);
+
+  factory SubjectModel.fromJson(Map<String, dynamic> json) {
+    return SubjectModel(
+      name: json[SubjectModelFields.NAME],
+      code: json[SubjectModelFields.CODE],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      SubjectModelFields.NAME: name,
+      SubjectModelFields.CODE: code
+    };
+  }
 }
--- test/features/timetable/data/models/subject_model_test.dart
+++ test/features/timetable/data/models/subject_model_test.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pretend/features/timetable/data/models/subject_model.dart';
 import 'package:pretend/features/timetable/domain/entities/subject.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
 
 void main() {
   final tSubjectModel = SubjectModel(
@@ -8,7 +12,28 @@ void main() {
     code: "IT-502",
   );
 
-  test("should be a subclass of Subject entity", () {
+  test('should be a subclass of Subject entity', () {
     expect(tSubjectModel, isA<Subject>());
   });
+
+  test(
+    'should return a valid SubjectModel',
+    () async {
+      final Map<String, dynamic> jsonMap = json.decode(fixture("subject.json"));
+      final result = SubjectModel.fromJson(jsonMap);
+      expect(result, tSubjectModel);
+    },
+  );
+
+  test(
+    'should return a JSON map containing proper data',
+    () async {
+      final result = tSubjectModel.toJson();
+      final expectedJsonMap = {
+        SubjectModelFields.NAME: "Computer Networks",
+        SubjectModelFields.CODE: "IT-502"
+      };
+      expect(result, expectedJsonMap);
+    },
+  );
 }
