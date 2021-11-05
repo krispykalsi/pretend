dff298427cab99df9ffa9bed7e921942f060a011
Ikroop Singh Kalsi <ikroop.singh.kalsi@gmail.com>
2021-11-05 14:14:55 +0530

    refactor failure to message

--- lib/application/bloc/setup/subjects/subjects_bloc.dart
+++ lib/application/bloc/setup/subjects/subjects_bloc.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:pretend/core/error/failures.dart';
 import 'package:pretend/core/network/data_source_enum.dart';
 import 'package:pretend/domain/entities/subject.dart';
 import 'package:pretend/domain/usecases/get_all_subjects.dart';
@@ -25,20 +24,11 @@ class SubjectsBloc extends Bloc<SubjectsEvent, SubjectsState> {
         await _getAllSubjects.call(GetAllSubjectsParams(DataSource.NETWORK));
     yield* subs.fold(
       (failure) async* {
-        yield Error(msg: _mapFailureToMessage(failure));
+        yield Error(msg: failure.message);
       },
       (subs) async* {
         yield Loaded(subjects: subs);
       },
     );
-  }
-
-  String _mapFailureToMessage(Failure failure) {
-    switch (failure.runtimeType) {
-      case ServerFailure:
-        return SERVER_FAILURE_MESSAGE;
-      default:
-        return UNEXPECTED_FAILURE_MESSAGE;
-    }
   }
 }
--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -1,20 +1,29 @@
 import 'package:equatable/equatable.dart';
 
 abstract class Failure extends Equatable {
-  final List properties;
-  // If the subclasses have some properties, they'll get passed to this constructor
-  // so that Equatable can perform value comparison.
-  Failure([this.properties = const <dynamic>[]]);
+  final String message = "Unexpected error occurred";
 
   @override
-  List<Object?> get props => properties;
+  List<Object?> get props => [message];
 }
 
-class NoLocalDataFailure extends Failure {}
-class NoInternetFailure extends Failure {}
-class ServerFailure extends Failure {}
-class CacheFailure extends Failure {}
+class NoLocalDataFailure extends Failure {
+  @override
+  String get message => "No data found in local storage";
+}
+class NoInternetFailure extends Failure {
+  @override
+  String get message => "No internet connectivity found";
+}
+class ServerFailure extends Failure {
+  @override
+  String get message => "Server Failure";
+}
+class CacheFailure extends Failure {
+  @override
+  String get message => "Cache Failure";
+}
 
-const String SERVER_FAILURE_MESSAGE = 'Server Failure';
-const String CACHE_FAILURE_MESSAGE = 'Cache Failure';
-const String UNEXPECTED_FAILURE_MESSAGE = 'Unexpected error occurred';
+// const String SERVER_FAILURE_MESSAGE = 'Server Failure';
+// const String CACHE_FAILURE_MESSAGE = 'Cache Failure';
+// const String UNEXPECTED_FAILURE_MESSAGE = 'Unexpected error occurred';
